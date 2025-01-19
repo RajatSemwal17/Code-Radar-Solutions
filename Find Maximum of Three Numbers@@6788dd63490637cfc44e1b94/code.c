f7c56b65050463e8419d8ebb6a609caab0b708b6
RajatSemwal17 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-19 10:57:58 +0530

    Solution to Find Maximum of Three Numbers

--- Find Maximum of Three Numbers@@6788dd63490637cfc44e1b94/code.c
+++ Find Maximum of Three Numbers@@6788dd63490637cfc44e1b94/code.c
@@ -2,13 +2,13 @@
 
 int greatest(int a,int b,int c) {
    if(a>b && a>c){
-    return "a";
+    return a;
    }
    else if(b>a && b>c){
-    return "b";
+    return b;
    }
    else{
-    return "c";
+    return c;
    }
 }
 
