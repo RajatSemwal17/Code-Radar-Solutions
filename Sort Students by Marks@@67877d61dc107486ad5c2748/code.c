7ffb3b46124722076109ea98fbbc2e24e5b6c9c6
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-21 21:25:12 +0530

    Solution to Sort Students by Marks

---
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct info{
+    int rollno;
+    char name[40];
+    float marks;
+};
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    struct info e[n];
+    for(int i=0;i<n;i++){
+        scanf("%d %s %f",&e[i].rollno,e[i].name,&e[i].marks);
+    }
+    for(int i=0;i<n;i++){
+        printf("Roll Number: %d, Name: %s, Marks: %.2f",e[i].rollno,e[i].name,e[i].marks);
+    }
+    return 0;
+}
