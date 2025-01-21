d561eba5cd680888900ad63566d130e2f2ec52ac
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-21 21:45:53 +0530

    Solution to Check if All Students Passed

---
+++ Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct info{
+    int rollno;
+    char name[30];
+    float marks;
+};
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    struct info e[n];
+    for(int i=0;i<n;i++){
+        scanf("%d %s %f",&e[i].rollno,e[i].name,&e[i].marks);
+    }
+    return 0;
+}
