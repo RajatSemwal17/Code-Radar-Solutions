d5714009bc03a562d8ef3adfc7c1b28f114728ca
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-21 19:49:15 +0530

    Solution to Count Students Scoring Above a Threshold

--- Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
+++ Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
@@ -15,7 +15,7 @@ int main() {
         scanf("%d %s %f",&e[i].rollno,e[i].name,&e[i].marks);
     }
     scanf("%f",&maxMarks);
-    for(int i=0=;i<n;i++){
+    for(int i=0;i<n;i++){
         if(maxMarks > e[i].marks){
             printf("Count of students scoring above %.2f: %d",maxMarks,count);
             count++;
