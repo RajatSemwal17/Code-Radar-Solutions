9a30d50d41a72f9dbb0fe831d3733a271311c0e0
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-21 22:26:23 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -20,8 +20,9 @@ int main() {
             correct=1;
         }
     }
+    for(int i=0;i<n;i++){
     if(correct){
         printf("Books above price %.2f: Title: %s, Author: %s, Price: %.2f",maxPrice,e[i].Book,e[i].Author,e[i].price);
-    }
+    }}
     return 0;
 }
