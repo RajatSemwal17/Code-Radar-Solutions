166a8123d98369745a8c243dd32d3b1a4f680fca
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-21 21:42:32 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -19,8 +19,8 @@ int main() {
     for(int i=0;i<n;i++){
         scanf("%d %s %f",&e[i].rollno,e[i].name,&e[i].marks);
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             if(e[j].marks<e[j+1].marks){
                 Descending(&e[j],&e[j+1]);
             }
