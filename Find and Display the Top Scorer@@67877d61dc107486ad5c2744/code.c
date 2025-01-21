20173601844b8ed2f5e15a99ca92381e3e20bf98
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-21 14:55:26 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -9,7 +9,7 @@ struct info{
 int main() {
     int n;
     scanf("%d",&n);
-    struct info e;
+    struct info e[n];
     for(int i=0;i<n;i++){
         scanf("%d %s %f",&(e[i].rollno),e[i].name,&(e[i].marks));
     }
