27d17cdefb6889d207742b656e079bb609e90c7a
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-22 15:18:52 +0530

    Solution to Reverse a String

--- Reverse a String@@676c039d80e1714600f67640/code.c
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -4,7 +4,7 @@
 void ReverseString(char str[]){
     int len = strlen(str);
     char temp;
-    for(int i=0;i<=n/2;i++){
+    for(int i=0;i<=len/2;i++){
         temp = str[i];
         str[i] = str[n-i-1];
         str[n-i-1] = temp;
