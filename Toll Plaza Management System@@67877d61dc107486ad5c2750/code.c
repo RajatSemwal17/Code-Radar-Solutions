783691d581e5bf7c1b5444987ba547b848c866b1
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-22 14:03:27 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct info{
+    int num;
+    char name[30];
+    float price;
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    struct info e[n];
+    for(int i=0;i<n;i++){
+        scanf("%d %s %f",&e[i].num,e[i].name,&e[i].price);
+    }
+    for(int i=0;i<n;i++){
+        printf("%s: %.2f",e[i].name,e[i].price);
+    }
+    return 0;
+}
