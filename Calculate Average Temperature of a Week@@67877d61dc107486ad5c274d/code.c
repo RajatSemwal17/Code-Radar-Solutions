a13a8c38ad1f7347d52065c3e09cbb3273f1e716
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-22 11:12:10 +0530

    Solution to Calculate Average Temperature of a Week

--- Calculate Average Temperature of a Week@@67877d61dc107486ad5c274d/code.c
+++ Calculate Average Temperature of a Week@@67877d61dc107486ad5c274d/code.c
@@ -6,13 +6,13 @@ struct week{
 
 int main() {
     struct week e[7];
-    float totalTemp=0,average;
+    float totalTemp=0 , average;
     for(int i=0;i<7;i++){
         scanf("%f",&e[i].temp);
 
         totalTemp += e[i].temp;
     }
-    average = temp/7;
+    average = totalTemp/7;
     printf("Average Temperature: %.2f",average);
     return 0;
 }
