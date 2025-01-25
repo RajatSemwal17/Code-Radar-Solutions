4d1778e6ade78f4dd13e71b3c4f2f8130c201c8a
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-25 18:58:11 +0530

    Solution to Find the First Peak Element

---
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int firstMaxNumber(int arr[],int n){
+    if(n==1){
+        return arr[0];
+    }
+    if(arr[0] > arr[1]){
+        return arr[0];
+    }
+    for(int i=0;i<n-1;i++){
+        if(arr[i] > arr[i+1] && arr[i] > arr[i-1]){
+            return arr[i]
+        }
+    }
+    if(arr[n-1]>arr[n-2]){
+        return arr[n-1];
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int peakNum = firstMaxNumber(arrr,n);
+    if(peakNum){
+        printf("%d",peakNum);
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
