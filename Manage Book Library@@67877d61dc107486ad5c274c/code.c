a5a062e7efc50ac49f59676f5e8eb041125349f9
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-01-25 18:07:00 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -10,12 +10,17 @@ int main() {
     int n;
     scanf("%d",&n);
     struct library e[n];
-    printf("Books above price %.2f:\n",maxPrice);
+    
     for(int i=0;i<n;i++){
     scanf("%s %s %f",e[i].Book,e[i].Author,&e[i].price);
+    }
 
     float maxPrice;
     scanf("%f",&maxPrice);
+
+    printf("Books above price %.2f:\n",maxPrice);
+    for(int i=0;i<n;i++){
+
         if(e[i].price > maxPrice){
         printf("Title: %s, Author: %s, Price: %.2f\n",e[correct].Book,e[correct].Author,e[correct].price);
         }
