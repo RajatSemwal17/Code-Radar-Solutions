d7910ad3fe258f7bb81bfc9020df7ae4d586aa59
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-02-06 22:04:18 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
 
-void rotateArray(int arr[],int n,int rotate){
-    for(int i=rotate;i<n;i++){
-        printf("%d\n",arr[i]);
+void rotateArray(int arr[], int n, int rotate) {
+    // Print elements from rotate index to end
+    for (int i = rotate; i < n; i++) {
+        printf("%d\n", arr[i]);
     }
-    for(int i=0;i<rotate;i++){
-        printf("%d\n",arr[i]);
+    // Print elements from start to rotate-1
+    for (int i = 0; i < rotate; i++) {
+        printf("%d\n", arr[i]);
     }
 }
 
 int main() {
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int rotate;
-    scanf("%d",&rotate);
+    int n, rotate;
 
-    rotateArray(arr,n,rotate);
+    // Input array size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input rotation index
+    scanf("%d", &rotate);
+
+    // Validate rotation index
+    if (rotate < 0 || rotate >= n) {
+        printf("Invalid index!\n");
+        return 1;
+    }
+
+    // Rotate and print the array
+    rotateArray(arr, n, rotate);
 
     return 0;
 }
