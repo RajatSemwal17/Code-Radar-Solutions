d8d7c1163334ed89946c5f471a1ccf67b7f3903b
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-02-16 17:03:08 +0530

    Solution to Convert Decimal to Binary

--- Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
+++ Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
@@ -1,28 +1,58 @@
-#include <stdio.h>
-
-void convertDecToBinary(int n);
-void convertDecToBinary(int n){
-    int arr[32];
-    int i=0;
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int binary[32];
     if(n==0){
         printf("0");
-        return;
     }
-
+    int i=0;
     while(n>0){
-        arr[i] = n%2;
+        binary[i] = n%2;
         n/=2;
         i++;
     }
-    for(int j=i-1;j>=0;j--){
-        printf("%d",arr[j]);
+    for (int j = i-1; j >=0; j--)
+    {
+        printf("%d",binary[j]);
     }
-
+    
 }
 
-int main() {
-    int n;
-    scanf("%d",&n);
-    convertDecToBinary(n);
-    return 0;
-}
+
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+
+// void convertDecToBinary(int n);
+// void convertDecToBinary(int n){
+//     int arr[32];
+//     int i=0;
+//     if(n==0){
+//         printf("0");
+//         return;
+//     }
+
+//     while(n>0){
+//         arr[i] = n%2;
+//         n/=2;
+//         i++;
+//     }
+//     for(int j=i-1;j>=0;j--){
+//         printf("%d",arr[j]);
+//     }
+
+// }
+
+// int main() {
+//     int n;
+//     scanf("%d",&n);
+//     convertDecToBinary(n);
+//     return 0;
+// }
