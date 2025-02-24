15dad3d69acb7c1a8d494fed51393bd74652be1a
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-02-24 23:01:23 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,9 +5,9 @@ int main() {
     char c;
     scanf("%d %d %c",&a,&b,&c);
 
-    if(c=='+') printf("%d",a+b);
-    if(c=='-') printf("%d",a-b);
-    if(c=='/') printf("%d",a/b);
-    if(c=='*') printf("%d",a*b);
+    if(c='+') printf("%d",a+b);
+    if(c='-') printf("%d",a-b);
+    if(c='/') printf("%d",a/b);
+    if(c='*') printf("%d",a*b);
     return 0;
 }    
