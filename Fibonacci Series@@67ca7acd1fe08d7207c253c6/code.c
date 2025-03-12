0840822111cb883af5b3b19fd9e893674caae48d
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-03-12 15:22:41 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,9 @@
 void fibonacciSeries(int n){
-    if(n==0)  printf("0");
-    if(n==1) printf("1");
-    if(n==2) printf("1");
-    printf("%d",fibonacciSeries(n-1) + fibonacciSeries(n-2));
+    int a=0;
+    int b=1;
+    int t = a+b;
+    for(int i=0;i<n;i++){
+        t=a+b;
+        printf("%d ",arr[i]);
+    }
 }
