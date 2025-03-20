520ff1362c481e989232e2b164898ad4ae4fc8f1
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-03-20 22:12:39 +0530

    Solution to Find the Pair with the Minimum Difference

---
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<limits.h>
+int pairWithMinDifference(int arr[], int n){
+    int minDiff = INT_MAX;
+    int index1 = -1, index2 = -1;
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int diff = abs(arr[j]-arr[i]);
+            if(diff < minDiff){
+                minDiff = diff;
+                index1 = i;
+                index2 = j;
+            }
+        }
+    }
+    printf("%d %d",index1,index2);
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    pairWithMinDifference(arr,n);
+}
