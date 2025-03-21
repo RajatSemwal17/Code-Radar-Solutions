51de186e66828dad71554355cfaf83f22e828022
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-03-21 11:47:31 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,28 +1,65 @@
 #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     int count;
+//     int freq[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//         freq[i] = -1;
+//     }
+//     for(int i=0;i<n;i++){
+//         count=1;
+//         for(int j=i+1;j<n;j++){
+//             if(arr[i]==arr[j]){
+//                 count++;
+//                 freq[j] = 0;
+//             }
+//     }
+//             if(freq[i] != 0){
+//                 freq[i] = count;
+//             }
+// }
+//     for(int i=0;i<n;i++){
+//         if (freq[i] != 0) printf("%d %d\n",arr[i],freq[i]);
+//     }
+//     return 0;
+// }
+
+
+
+
+#include<stdio.h>
 int main(){
     int n;
     scanf("%d",&n);
     int arr[n];
-    int count;
-    int freq[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
-        freq[i] = -1;
     }
+    int hash[100];
     for(int i=0;i<n;i++){
-        count=1;
-        for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]){
-                count++;
-                freq[j] = 0;
-            }
+        hash[arr[i]]++;
     }
-            if(freq[i] != 0){
-                freq[i] = count;
-            }
-}
+
     for(int i=0;i<n;i++){
-        if (freq[i] != 0) printf("%d %d\n",arr[i],freq[i]);
+        printf("%d %d",hash[i],i);
     }
-    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
