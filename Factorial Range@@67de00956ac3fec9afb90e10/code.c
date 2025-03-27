30734ce30c57443717cc65428325a3e7edea075c
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-03-27 18:35:46 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,9 @@
+factorialRange(int start,int end){
+    for(int i = start;i<=end;i++){
+        int product = 1;
+        for(int j = 1;j<n;j++){
+            product*=j;
+        }
+        printf("%d ",product);
+    }
+}
