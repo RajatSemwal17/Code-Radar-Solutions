054235584bab3dcd4ad2d9fb538a6788e42a4475
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-03-30 19:35:49 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -25,10 +25,12 @@ void pairWithMinDifference(int arr[],int n){
         }
     }
 
-    if(a<b){printf("%d %d\n",a,b);}
-    else if(b<a){printf("%d %d\n",b,a);}
+    int g = gcd(a,b);
+
+    if(a<b){printf("%d %d\n",a/g,b/g);}
+    else if(b<a){printf("%d %d\n",b/g,a/g);}
     else if(a==0 || b==0){printf("-1");}
-    else {printf("%d %d\n",b,a);}
+    else {printf("%d %d\n",b/g,a/g);}
 }
 
 int main(){
