c7bbf3d0c5f967fc1c95ed9f6c907e2114f55ca2
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-03-30 19:15:11 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -7,8 +7,9 @@ int leadersInArray(int arr[],int n){
         arr[i] = maxi;
         if(temp > maxi){
             maxi = temp;
-            break;
+            
         }
+        break;
     }
 }
 
