68451d00376bcc723ec2c8f325b8393ffba7a3e4
RS-CODER💻💀 <195555089+RajatSemwal17@users.noreply.github.com>
2025-03-30 17:44:59 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -5,8 +5,8 @@ int MedianOfArray(int arr[],int n){
     int sum=0;
     for(int i=0;i<n;i++){
         sum+=arr[i];
-        median = sum/n;
     }
+        median = sum/n;
     return median;
 }
 int main(){
